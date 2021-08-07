6bd9c057e3ad4e75002f39c5285228a12d3ed2e8
Liang-Chi Hsieh <viirya@gmail.com>
2021-08-07 14:37:13 -0700

    Add cpp code.

---
+++ cpp/parquet_arrow/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <arrow/api.h>
+#include <arrow/result.h>
+#include <arrow/io/api.h>
+#include <parquet/arrow/reader.h>
+#include <parquet/exception.h>
+
+void read_parquet_file(std::string parquet_filename) {
+    std::cout << "Reading " << parquet_filename << " at once" << std::endl;
+    arrow::Result<std::shared_ptr<arrow::io::ReadableFile>> infileResult = arrow::io::ReadableFile::Open(
+            parquet_filename, arrow::default_memory_pool());
+    PARQUET_THROW_NOT_OK(infileResult);
+
+    std::shared_ptr<arrow::io::ReadableFile> infile = infileResult.ValueUnsafe();
+
+    std::unique_ptr<parquet::arrow::FileReader> reader;
+    PARQUET_THROW_NOT_OK(
+            parquet::arrow::OpenFile(infile, arrow::default_memory_pool(), &reader));
+
+    std::unique_ptr<arrow::RecordBatchReader> recordBatchReader;
+
+    std::vector<int> rowGroup;
+    for(int i = 0; i < reader->num_row_groups(); i++) {
+        rowGroup.push_back(i);
+    }
+    PARQUET_THROW_NOT_OK(reader->GetRecordBatchReader(rowGroup, &recordBatchReader));
+
+    long accu1 = 0;
+    long accu2 = 0;
+    while (true) {
+        arrow::Result<std::shared_ptr<arrow::RecordBatch>> next = recordBatchReader->Next();
+
+        if (next.ok()) {
+            std::shared_ptr<arrow::RecordBatch> nextRecordBatch = next.ValueUnsafe();
+            // Even `Next` result is okay, the record batch could possible be null...
+            if (nextRecordBatch != NULL) {
+                std::shared_ptr<arrow::ArrayData> data = next.ValueOrDie()->column(0)->data();
+                int num_rows = data->length;
+
+                std::cout << "num rows: " << num_rows << std::endl;
+                for (int i = 0; i < num_rows; ++i) {
+                    std::shared_ptr<arrow::Scalar> scalar = next.ValueOrDie()->column(0)->GetScalar(i).ValueUnsafe();
+                    // std::cout << scalar->ToString() << std::endl;
+                    // std::cout << scalar.get()->type.get()->ToString() << std::endl;
+
+                    accu1 += (long) *(int32_t*)((arrow::Int32Scalar*) scalar.get())->data();
+                }
+                std::cout << "accu1: " << accu1 << std::endl;
+            } else {
+                return;
+            }
+        } else {
+            return;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        std::cout << "parquet_arrow takes one parquet filename as input." << std::endl;
+    } else {
+        read_parquet_file(std::string(argv[1]));
+    }
+}
